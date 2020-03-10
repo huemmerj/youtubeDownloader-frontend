7c7a399ec2e5d10c5942666f5c1cca3a808456b4
Hümmer Jens <j.huemmer@fis-gmbh.de>
2020-03-10 12:27:11 +0100

    new

--- src/components/Select-Choose-Format.vue
+++ src/components/Select-Choose-Format.vue
@@ -3,8 +3,8 @@
     <div class="input-group-prepend">
       <label class="input-group-text" for="inputGroupSelect01">Format</label>
     </div>
-    <select v-model="format"class="custom-select" id="inputGroupSelect01">
-      <option value="1">Mp3(Audio)</option>
+    <select :value="format" ref="format" class="custom-select" id="inputGroupSelect01" @input="updateFormat">
+      <option selected value="1">Mp3(Audio)</option>
       <option value="2">Mp4(Video)</option>
     </select>
   </div>
@@ -15,8 +15,11 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'SelectChooseFormat',
-  props: {
-    format: Boolean,
-  },
+  props: ['format'],
+  methods: {
+    updateFormat() {
+      this.$emit('input', this.$refs.format.value)
+    }
+  }
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
-<template>
-  <div class="home">
-    <choose-format/>
+<template> 
+  <div class="container-fluid">
+    <div class="row">
+      <div class="btn-group mr-2 mb-3" role="group" aria-label="First group">
+        <button @click="setSingle" type="button" class="btn" :class="{ 'btn-primary': !playlist, 'btn-secondary': playlist}" >Einzeln</button>
+        <button @click="setPlaylist" type="button" class="btn" :class="{ 'btn-primary': playlist, 'btn-secondary': !playlist} ">Playlist</button>
+      </div>
+      <choose-format v-model="format"/>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="url">URL</label>
+        </div>
+        <input id="url" class="form-control" v-model="url" placeholder="https://www.youtube.com/watch?v=6Lrmy8-p3BM" />
+      </div>
+      <div v-if="!playlist" class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="fileName">Name </label>
+        </div>
+        <input id="fileName" class="form-control" v-model="fileName"/>
+      </div>
+      <button class="btn btn-primary">Download</button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +29,31 @@ export default {
   name: 'Home',
   components: {
     "choose-format": SelectChooseFormat
+  },
+  computed: {
+    filter: function () {
+      if (format==="1") {
+        return 'audioonly'
+      } else {
+        return ''
+      }
+    },
+  },
+  data () {
+    return {
+      format: '1',
+      url: '',
+      fileName: '',
+      playlist: false
+    }
+  },
+  methods: {
+    setSingle() {
+      this.playlist = false
+    },
+    setPlaylist() {
+      this.playlist = true
+    }
   }
 };
 </script>
