2841f98c32ba18710050395d3efe32d7ea6344f8
Hümmer Jens <j.huemmer@fis-gmbh.de>
2020-03-10 11:25:37 +0100

    Initial Commit

---
+++ src/components/Select-Choose-Format.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <label class="input-group-text" for="inputGroupSelect01">Format</label>
+    </div>
+    <select v-model="format"class="custom-select" id="inputGroupSelect01">
+      <option value="1">Mp3(Audio)</option>
+      <option value="2">Mp4(Video)</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SelectChooseFormat',
+  props: {
+    format: Boolean,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <choose-format/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SelectChooseFormat from '@/components/Select-Choose-Format'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    "choose-format": SelectChooseFormat
   }
-}
+};
 </script>
