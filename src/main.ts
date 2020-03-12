2aa6f14d77533bd77111eafdd5e4595286c4f78a
huemmerj <j.huemmer@fis-gmbh.de>
2020-03-12 08:46:25 +0100

    f

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import axios from 'axios'
+import axios from 'axios';
+import VueSocketIO from 'vue-socket.io';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:4000',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_',
+  },
+}))
+
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios
 new Vue({
